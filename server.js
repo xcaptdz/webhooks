ffd5a6b1875705e98370037f3c48d442871cfcc9
xcaptdz <147160098+xcaptdz@users.noreply.github.com>
2023-10-20 17:51:44 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,57 +1,16 @@
+
+//gitrequire('dotenv').config()
+
 const express = require('express')
 const app = express()
-const request = require('request');
 
-app.get('/',(req,res)=>{
-   
-    
-    let hook = req.query.hook;
-    
-    if ((hook != null ) && (hook != "") ){
-     res.status(200).send(hook)
+//const request = require('request');
 
-     let ip = '10.0.20.6'
-      
-     if (process.env.HA_IP != null && process.env.HA_IP != '' ) {
-        ip = process.env.HA_IP;
-    
-    }
+app.use(express.json())
 
 
-
-      request.post({ headers: {'content-type' : 'text/html'}
-     , url: 'http://'+ip+':8123/api/webhook/'+hook,
-      body: "" }
-     , function(error, response, body){
-     console.log(hook)
-     console.log(ip)
-     }); 
- 
-           
-    } else  {
-
-        res.status(200).send("Welcome to WebHooks Router for HA!")
-    }
-
-   
-  
-
-
-
-  
-})
-
-
+const webhooksRouter = require('./routes/webhooks')
+app.use('/webhooks',webhooksRouter)
  
 
-app.get('*',(req,res)=>{
-
-    res.status(200).send("Welcome to WebHooks Router for HA!")
-
-})
-
-
-
-
-
-app.listen(5000)
+app.listen(5000, ()=> console.log("Server Started Port: 5000"))
